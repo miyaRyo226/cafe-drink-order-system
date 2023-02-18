894fd63b13c9d3507ee89b29f94adc6933ce71d7
ryoya <ryoya.miyamoto226@gmail.com>
2023-02-18 05:40:46 +0000

    add メニュボタンの要素用コンポーネント作成

---
+++ src/components/pages/DrinkOrder/MenuButton/Content/index.tsx
@@ -0,0 +1,11 @@
+import { FC } from 'react'
+
+type Props = { menuName: string; price: number }
+export const MenuContent: FC<Props> = ({ menuName, price }) => {
+	return (
+		<div className='gap-8 flex flex-row justify-between'>
+			<div className='w-fit'>{menuName}</div>
+			<div className='w-fit grow text-right'>{price}円</div>
+		</div>
+	)
+}
